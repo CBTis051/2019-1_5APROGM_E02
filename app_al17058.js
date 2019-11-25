ef75de18ecaeddf02624a55ebb073122e4434462
Enzo Alejandro Maciel Núñez <al17058@cbtis051.edu.mx>
2019-11-25 11:07:14 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17058.js
+++ app_al17058.js
@@ -1,7 +1,15 @@
 var http = require('http');
 http.createServer(function (req,res){
+
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo JS Repo GIT - Maciel Núñez Enzo Alejandro\n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Maciel Núñez Enzo Alejandro'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+    
 }).listen(8081);
 console.log('Server running on port 8081');
