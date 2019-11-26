f4cd3cd1b904123fd358aa8d20a8ac9dbdba7933
al17195 <55166781+al17195@users.noreply.github.com>
2019-11-25 23:26:15 -0700

    Delete app_al17195.js

--- app_al17195.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req,res){
-    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-
-    res.end('Hola mundo JS Repo GIT - Espinoza Barrera Karla Lizeth\n');
-}).listen(8081);
-console.log('Server running on port 8081');
