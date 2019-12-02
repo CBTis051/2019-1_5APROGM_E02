334205cf21ea447e97c6cbe4962b1ad73d66b71e
Angelica Esthefania Bañuelos Devora <al17187@cbtis051.edu.mx>
2019-12-01 22:39:01 -0600

    Ajustes en la respuesta a formato JSON

--- app_al17187.js
+++ app_al17187.js
@@ -1,7 +1,15 @@
 var http = require ('http');
 http.createServer(function (req, res) {
+
 	res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+
+	var objeto = {
+		mensaje: 'Hola mundo Node JS Repo GIT - Bañuelos Devora Angelica Esthefania\n'
+	};
 	
-	res.end('Hola mundo Node JS Repo GIT - Bañuelos Devora Angelica Esthefania\n');
+	var json = JSON.stringify(objeto);
+
+	res.end(json);
+
 }).listen(8081);
 console.log('Server running on port 8081');
