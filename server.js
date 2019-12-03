89bc5e07bf7655ac5afc7cfc4d4fe7504e1f4eca
Enzo Alejandro Maciel Núñez <al17058@cbtis051.edu.mx>
2019-12-03 10:04:33 -0700

    paso 19

---
+++ server.js
@@ -0,0 +1,38 @@
+require('./config/config');
+
+const express = require('express');
+
+const app = express();
+
+
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Configuración global de rutas
+app.use(require('./routes/index'));
+
+/**
+ * Home del server --> / >_<XD
+ */
+app.get('/', (req, res) => {
+
+    let objeto = {
+        mensaje: "Bienvenidos al REST server - 5APROGM Equipo 02"
+    };
+
+    res.send(objeto);
+
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port ", process.env.PORT);
+});
